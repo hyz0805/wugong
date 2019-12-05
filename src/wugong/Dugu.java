d428b7bdaa5b52c232f7e0bb1ea3a414fc46eec6
wxy622827 <3260269944@qq.com>
2019-12-05 12:36:48 +0800

    33

--- src/wugong/Dugu.java
+++ src/wugong/Dugu.java
@@ -5,7 +5,8 @@ public class Dugu {
 	public static void main(String[] args) {
        System.out.println("11111");
        System.out.println("22222 ");
-       System.out.println("22222 ");
+       System.out.println("33333 ");
+       
 }
 
 }
