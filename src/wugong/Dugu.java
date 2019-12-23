a2f78c1d4d6121b27735c588a5913ac71b97fd27
heyingzhao <2968877080@qq.com>
2019-12-23 13:04:35 +0800

    22

--- src/wugong/Dugu.java
+++ src/wugong/Dugu.java
@@ -3,7 +3,7 @@ package wugong;
 public class Dugu {
 
 	public static void main(String[] args) {
-       System.out.println("11111");
+       System.out.println("11111111");
        System.out.println("22222 ");
        System.out.println("33333");
 }
